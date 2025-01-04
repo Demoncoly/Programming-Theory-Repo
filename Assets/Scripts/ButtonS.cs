6438d56097de50c14a73f0f24a187b7ef7acfcd5
Demoncoly <figgy345@gmail.com>
2025-01-04 03:57:51 -0500

    character select
    added screen with buttons,
    added simple script that changes scene to game scene

---
+++ Assets/Scripts/ButtonS.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonS : MonoBehaviour
+{
+public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+public void SelectCar()
+    {
+
+    }
+public void SelectPerson()
+    {
+
+    }
+}
